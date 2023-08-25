df2aad4ff025f9ad2f67923175732014544e6d9e
AmanFromOmsk <amanrahim2@gmail.com>
2023-08-26 04:25:38 +0600

    resize table

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,33 @@
-import { ExcelComponent } from "@core/ExcelComponent";
-import { createTable } from "./table.template";
+import {ExcelComponent} from '@core/ExcelComponent'
+import {createTable} from '@/components/table/table.template'
+import {resizeHandler} from '@/components/table/table.resize'
+import {shouldResize} from '@/components/table/table.functions'
 
 export default class Table extends ExcelComponent {
-    static className = 'excel__table'
+  static className = 'excel__table'
 
-    toHTML() {
-        return createTable(20)
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown']
+    })
+  }
+
+  toHTML() {
+    return createTable(20)
+  }
+
+  onMousedown(event) {
+    if (shouldResize(event)) {
+     resizeHandler(this.$root, event)
     }
+  }
 }
+
+// 589 msScripting
+// 2433 msRendering
+
+// 440 msScripting
+// 1771 msRendering
+
+// 125 msScripting
+// 329 msRendering
